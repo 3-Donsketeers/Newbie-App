ac42993ff8f4a5ce0e1db33585284ec4c5288b39
ndullagh <113929865+ndullagh@users.noreply.github.com>
2024-07-12 11:00:35 -0700

    added Hez's about/contact text

--- packages/react-frontend/src/pages/about.js
+++ packages/react-frontend/src/pages/about.js
@@ -7,14 +7,50 @@ export default function About() {
     <>
       <Navigation />
       <div className="page public-sans">
-        <h1>About</h1>
-        <p> 
-            <ul>
-                <li>Introducing Newbie Hub, the software designed to help you navigate the U.S. Department of Agriculture (USDA)! Our objective is to offer a comprehensive and user-friendly website that provides novices with all the necessary knowledge they require to succeed in their USDA-related pursuits.</li>
-                <li>Newbie Hub is available to streamline your journey, whether you are a novice farmer, a food safety enthusiast, or someone who is investigating USDA programs for the first time. Our comprehensive range of materials includes detailed instructions, interactive utilities, and specialized guidance designed to meet your individual requirements. Our application encompasses a wide range of topics, including comprehending USDA laws, obtaining grants and loans, using sustainable agricultural methods, and participating in nutrition programs.</li>
-                <li>At Newbie Hub, we hold the belief that every individual should have access to dependable and comprehensible knowledge. Our diligent team consistently enhances the app with the most recent knowledge and optimal methods, guaranteeing that you have the most up-to-date and precise information readily available.</li>
-                <li>Enroll in our network of individuals who are new to the USDA and begin your journey with assurance. Through Newbie Hub, you will not only receive information, but also have a supporting partner who is dedicated to ensuring your success. Discover a multitude of opportunities offered by the USDA, made accessible to you with the assistance of Newbie Hub.</li>
-            </ul>
+        <h1>About Us</h1>
+        <h5>About USDA</h5>
+        <p>
+          On May 15, 1862, President Abraham Lincoln signed legislation to establish the United States Department 
+          of Agriculture (USDA or the Department). Two and a half years later in his final message to Congress,
+          Lincoln called USDA “The People’s Department.” At that time, about half of all Americans lived on farms,
+          compared with about two percent today. But through our work on food, agriculture, economic development,
+          science, natural resource conservation, and a host of other issues, USDA continues to fulfill Lincoln’s
+          vision—serving millions of Americans every day. The agriculture and food industry contributes $1.1 trillion
+          to the U.S. domestic product and represents nearly 11% of total U.S. employment. Furthermore, USDA over
+          the years has gained more authorities to expand its support not only in agriculture but the support
+          infrastructure that give all producers better access to market and its surrounding communities and
+          natural resources to also thrive. Today, USDA is comprised of 29 agencies organized under eight Mission
+          Areas and 16 Staff Offices, with nearly 100,000 employees serving the American people at more than 6,000
+          locations across the country and abroad.
+        </p>
+        <h5>USDA Mission </h5>
+        <p>
+          To serve all Americans by providing effective, innovative, science-based public policy leadership
+          in agriculture, food and nutrition, natural resource protection and management, rural development,
+          and related issues with a commitment to deliverable equitable and climate-smart opportunities that inspire
+          and help America thrive
+        </p>
+        <h5>USDA Vision</h5> 
+        <p>
+          An equitable and climate-smart food and agriculture economy that protects and improves the health,
+          nutrition, and quality of life of all Americans; yields healthy land, forests, and clean water;
+          helps rural America thrive; and feeds the world.
+        </p>
+        <h5>USDA Core Values </h5>
+        <p>
+          <b>Respect and Dignity:</b> We treat all people with courtesy and respect, and we value the inherent dignity of every individual.
+        </p>
+        <p>
+          <b>Equity and Inclusion:</b> We seek to end discrimination in all forms, wherever it exists, and expand services and opportunities to underserved people and communities across America, starting with our workforce. 
+        </p>
+        <p>
+          <b>Trust and Integrity:</b> We act in a manner that is deserving of the public’s trust and with the utmost integrity in everything we do as public servants. 
+        </p>
+        <p>
+          <b>Service and Results:</b> We listen to our internal and external customers and actively incorporate their ideas on how to best reach our diverse customers and deliver service that significantly and positively impacts the lives of all Americans. 
+        </p>
+        <p>
+          <b>Science Leadership:</b> We base our decisions and policy on science and data that are reliable, timely, relevant and free from political interference.
         </p>
         {/* Add more content here */}
       </div>
--- packages/react-frontend/src/pages/contact.js
+++ packages/react-frontend/src/pages/contact.js
@@ -6,13 +6,24 @@ export default function Contact() {
   return (
     <>
       <Navigation />
-      <div className="page public-sans">
+      <div className="page public-sans center-text">
         <h1>Contact</h1>
-        <p> For assistance, please contact us at (888)888-8888
-        </p>
-        <p>OR</p>
-        <p>Contact us at our email at support@newbiehub.gov</p>
+        <h4>Customer Service Representatives are available via phone or live chat Monday to Friday from 8 AM – 5 PM EST</h4>
+        <p><b>Closed on Federal Holidays.</b></p>
+        <br />
+
+
+        <h4>Call Us</h4>
+        <p>Call us to speak with one of our Customer Service Representatives</p>
+
+        <b>CALL (833) ONE-USDA</b>
+        <b>(202) 720-2791</b>
+        <br />
+        <h4>Email Us</h4>
+        <p>Send your questions via email and receive a response within 1-2 business days.</p>
         
+        <p><b>askusda@usda.gov</b></p>
+
         {/* Add more content here */}
       </div>
     </>
